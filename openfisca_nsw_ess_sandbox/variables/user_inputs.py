f756709c098efc5eeff059073e2a4c6068cda9b4
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-02-10 13:44:19 +1100

    Create minimal working example

--- openfisca_nsw_ess_sandbox/variables/user_inputs.py
+++ openfisca_nsw_ess_sandbox/variables/user_inputs.py
@@ -1,48 +1,44 @@
+import numpy as np
+
 # Import from openfisca-core the common Python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
+
 # Import the Entities specifically defined for this tax and benefit system
-from openfisca_nsw_base.entities import *
+from openfisca_nsw_ess_sandbox.entities import *
+# from openfisca_nsw_base.entities import *
+
+class electricity_savings(Variable):
+    value_type = float
+    # entity = Building
+    entity = Appliance
+    default_value = 0
+    definition_period = YEAR
+    label = 'What is the electricity savings per year?'
 
 
-# class F1ProductClass(Enum):
-#     product_class_one = 'RDC is in product class 1.'
-#     product_class_two = 'RDC is in product class 2.'
-#     product_class_three = 'RDC is in product class 3.'
-#     product_class_four = 'RDC is in product class 4.'
-#     product_class_five = 'RDC is in product class 5.'
-#     product_class_six = 'RDC is in product class 6.'
-#     product_class_seven = 'RDC is in product class 7.'
-#     product_class_eight = 'RDC is in product class 8.'
-#     product_class_nine = 'RDC is in product class 9.'
-#     product_class_ten = 'RDC is in product class 10.'
-#     product_class_eleven = 'RDC is in product class 11.'
-#     product_class_twelve = 'RDC is in product class 12.'
-#     product_class_thirteen = 'RDC is in product class 13.'
-#     product_class_fourteen = 'RDC is in product class 14.'
-#     product_class_fifteen = 'RDC is in product class 15.'
+class gas_savings(Variable):
+    value_type = float
+    # entity = Building
+    entity = Appliance
+    default_value = 0
+    definition_period = YEAR
+    label = 'What is the gas savings per year?'
 
 
-# class F1_product_class(Variable):
-#     value_type = Enum
-#     entity = Building
-#     possible_values = F1ProductClass
-#     default_value = F1ProductClass.product_class_one
-#     definition_period = ETERNITY
-#     label = 'What is the product class for the RDC installed in Activity' \
-#             ' Definition F1?'
+class number_of_certificates(Variable):
+    value_type = float
+    # entity = Building
+    entity = Appliance
+    definition_period = ETERNITY
+    label = 'Equation 1 of the ESS Rule 2009, used to calculate the number' \
+            ' of ESCs generated from a Recognised Energy Savings Activity.' \
+            ' As defined in Clause 6.5 of the ESS Rule 2009.'
 
+    def formula(self, period, parameters):
+        elec_savings = self('electricity_savings', period)
+        electricity_cert_conversion_factor = parameters(period).general_ESS.electricity_certificate_conversion_factor
 
-# class DutyClass(Enum):
-#     light_duty = 'cabinet is a light duty cabinet.'
-#     normal_duty = 'cabinet is a normal duty cabinet.'
-#     heavy_duty = 'cabinet is a heavy duty cabinet.'
+        gas_savings = self('gas_savings', period)
+        gas_cert_conversion_factor = parameters(period).general_ESS.gas_certificate_conversion_factor
 
-
-# class duty_class(Variable):
-#     value_type = Enum
-#     entity = Building
-#     possible_values = DutyClass
-#     default_value = DutyClass.light_duty
-#     definition_period = ETERNITY
-#     label = 'What is the duty class of the Refrigerated Cabinet, as tested in' \
-#             ' accordance with EN 16825?'
+        return np.floor((elec_savings * electricity_cert_conversion_factor) + (gas_savings * gas_cert_conversion_factor))
